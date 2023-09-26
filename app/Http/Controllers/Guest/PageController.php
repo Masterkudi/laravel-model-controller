52522140eaf9a66e4145c230fbae0137e7d6921c
masterkudi <cudini.andrea@gmail.com>
2023-09-26 20:01:00 +0200

    ho qualche problema a importare i dati

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -6,15 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Models\Movie;
 use Illuminate\Http\Request;
 
-class PageController extends Controller
-{
-    public function index () {
+class PageController extends Controller{
+    public function index() {
+
         $dati = Movie::all();
 
-        dd($dati);
-
-        return view('movies', [
-            "movies" => config("movies")
-        ]);
+        return view('homepage', [
+            "movies" => $dati
+        ]); 
     }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,14 +2,10 @@
 
 @section("content")
     <h1>Hello Folks!</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque beatae quas animi iusto enim magnam quo explicabo facilis repudiandae, error ipsa reiciendis rerum corrupti corporis amet, mollitia et a recusandae?
-    Labore et minima vel dolores, ex architecto animi, veritatis aliquid est facilis ad cupiditate optio ullam doloribus culpa adipisci commodi ducimus sequi exercitationem nobis praesentium esse! Mollitia quis rerum esse?
-    At non consectetur laboriosam asperiores, in saepe vitae quia reiciendis accusantium adipisci. Magnam qui quas labore delectus corrupti itaque. Ad obcaecati amet sint aut eos, ab reiciendis neque! Repellat, a.
-    Laborum deserunt repellendus adipisci, vel eum, sed repellat consequatur illum est dolores, cupiditate reiciendis qui laudantium unde. Quas itaque rem, ipsa mollitia commodi quam quae. Reprehenderit error repudiandae voluptatum perspiciatis.
-    Neque quaerat tempora officia vitae. In quisquam temporibus obcaecati ullam asperiores hic commodi quod molestiae voluptas fugiat similique voluptatibus delectus voluptate omnis, explicabo nemo perferendis cumque vitae alias? Eum, optio.
-    Mollitia laboriosam adipisci illo, nostrum velit dolorum ad fuga dolore similique placeat quisquam quasi enim, reprehenderit vitae est nisi nihil rem. Odit aliquam nemo voluptatum laboriosam qui voluptate fuga neque?
-    Iste magni error, et inventore quaerat iure voluptate aut ipsam odit cupiditate dolores atque mollitia, obcaecati facere! Sequi, reprehenderit rerum quo consequatur quis veritatis quibusdam? Eaque distinctio quia similique repellendus!
-    Ducimus fuga veniam ea iste eos ratione deserunt tempora officiis nulla aperiam quia sit eveniet minima odio quis, maxime blanditiis placeat perspiciatis, vero tempore. Impedit enim vitae accusamus mollitia maiores?
-    Quibusdam autem minima placeat facilis, culpa mollitia vitae quisquam hic nobis, pariatur voluptatibus, magnam earum exercitationem blanditiis id expedita aliquid asperiores? Inventore assumenda maxime magnam? Exercitationem molestiae voluptas qui debitis!
-    Voluptates ipsam consequatur odio dolor labore, deleniti porro reiciendis magni molestiae modi doloremque soluta beatae placeat fugiat voluptate maxime, blanditiis consequuntur culpa rerum quidem, dolores incidunt perferendis? In, vel tempora!</p>
+
+    <ul>
+        @foreach ($movies as $movie)
+            <li>{{ $movies->title}}</li>
+        @endforeach    
+    </ul>
 @endsection
