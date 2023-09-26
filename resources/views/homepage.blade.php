eee138326dc4001533e2b1712a05d47ef6b34594
masterkudi <cudini.andrea@gmail.com>
2023-09-26 18:09:01 +0200

    Aggiungo Controller

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,4 +2,5 @@
 
 @section("content")
     <h1>Hello Folks!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis libero numquam quod explicabo, nihil neque vitae repellendus, praesentium sapiente reprehenderit quos cupiditate ut eveniet facere minus aspernatur aut, perferendis voluptatum.</p>
 @endsection
